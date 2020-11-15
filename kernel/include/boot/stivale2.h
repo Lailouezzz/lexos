31582010cd649f77fa1dead8472a7fd36f04c0dc
Lailouezzz <alanlebouder@gmail.com>
2020-11-15 21:31:05 +0100

    Move limine binary, fix stack in stivale2hdr

--- kernel/include/boot/stivale2.h
+++ kernel/include/boot/stivale2.h
@@ -1,24 +1,25 @@
 #ifndef H_BOOT_STIVALE2
 #define H_BOOT_STIVALE2
+#include <stdint.h>
 
 
 struct stivale2_header {
-    unsigned long entry_point;   // If not 0, this address will be jumped to as the
+    uint64_t entry_point;   // If not 0, this address will be jumped to as the
                             // entry point of the kernel.
                             // If set to 0, the ELF entry point will be used
                             // instead.
 
-    unsigned long stack;    // This is the stack address which will be in ESP/RSP
+    uint64_t stack;         // This is the stack address which will be in ESP/RSP
                             // when the kernel is loaded.
                             // It can only be set to NULL for 64-bit kernels. 32-bit
                             // kernels are mandated to provide a vaild stack.
                             // 64-bit and 32-bit valid stacks must be at least 256 bytes
                             // in usable space and must be 16 byte aligned addresses.
 
-    unsigned long flags;    // Bit 0: if 1, enable KASLR
+    uint64_t flags;         // Bit 0: if 1, enable KASLR
                             // All other bits undefined
 
-    unsigned long tags;     // Pointer to the first of the linked list of tags.
+    uint64_t tags;          // Pointer to the first of the linked list of tags.
                             // see "stivale2 header tags" section.
                             // NULL = no tags.
 } __attribute__((packed));
--- kernel/src/boot/stivale2.c
+++ kernel/src/boot/stivale2.c
@@ -1,10 +1,11 @@
 #include "boot/stivale2.h"
 
 
+static uint8_t stack[4096] = { 0 };
 __attribute__((section(".stivale2hdr"), used))
 struct stivale2_header stivale2hdr = {
     .entry_point = 0,
-    .stack = 0,
+    .stack = (uintptr_t)stack + sizeof(stack),
     .flags = 0,
     .tags = 0
 };
